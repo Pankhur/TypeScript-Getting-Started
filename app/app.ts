dda929ecf04e879b005feed77f334bb8e1028fd0
pankhur <pankhurtulsyan@gmail.com>
2021-09-03 16:47:22 +0530

    Type annotation checks, correct way to write functions, Default initialised parameter and other learnings

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,34 @@
 function startGame() {
 
 	var message = document.getElementById('messages')
-	message!.innerText = " Hello Typescript"		//exclamation is aded to check if null
+	message!.innerText = " Hello Typescript"				//exclamation is aded to check if null
 
 
-}
+	
+	let showName: string = "Pankhur"						//strict type declaration to prevent other type 
+	logName(showName)
+
+
+
+    test();													//Default initialised parameter, function can be called with or without param
+    test("hi");
+
+    let receipt: number | string							//Union operator for variable declaration with multiple type
+    receipt = 1
+    receipt = "order1"
+   // receipt = true 											//this will give compilation error as boolean can't be assign to receipt variable
+
+	}
+
+	function logName(name) {
+	console.log(`Name of the person : ${name}`)
+
+ 	let value: any = 5
+	let myname: string = (<number>value).toFixed(4)
+	console.log("my name " + myname)
+
+	}
+
+	function test(value:string = "TEEST") {					//Type annotation "value:string" to function
+		console.log(`Default value: ${value}`)
+	}
--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-"use strict";
 function startGame() {
     var message = document.getElementById('messages');
     message.innerText = " Hello Typescript";
+    var showName = "Pankhur";
+    logName(showName);
+    test();
+    test("hi");
+}
+function logName(name) {
+    console.log("Name of the person : " + name);
+    var value = 5;
+    var myname = value.toFixed(4);
+    console.log("my name " + myname);
+}
+function test(value) {
+    if (value === void 0) { value = "TEEST"; }
+    console.log("Default value: " + value);
 }
 //# sourceMappingURL=app.js.map
